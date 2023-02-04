841e4cf03b029964fc047457f1154f8faa9e47cc
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2023-02-04 21:12:06 +0530

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,45 @@
-import React from "react";
-import { Table, Button } from "reactstrap";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Table, Card, CardBody, NavItem } from "reactstrap";
+import { ItemContext } from "../App";
 
 const Home = (props) => {
+  const { inventory } = useContext(ItemContext);
+
   return (
     <div>
-      <h2 className="App-header">Inventory List</h2>
+      <h2 className="App-header mb-3">Inventory List</h2>
       <div className="d-flex justify-content-center">
-        <Table striped className="w-50">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Item</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-            </tr>
-          </tbody>
-        </Table>
+        <Card className="w-50">
+          <CardBody>
+            <Table striped className="">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Item Name</th>
+                  <th>Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+                {inventory.map((item, index) => {
+                  return (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{item.name}</td>
+                      <td>{item.quantity}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+            <button className="btn btn-primary">
+              <Link className="nav-link" to="/lists">
+                Edit List
+              </Link>
+            </button>
+          </CardBody>
+        </Card>
       </div>
-      <Button>Edit List</Button>
     </div>
   );
 };
